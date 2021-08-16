3a3bad2f38fbe871e3cb0aecb2a32adf5031bb17
Tom Svilans <tom.svilans@gmail.com>
2021-08-16 22:46:50 +0200

    feat(rawlam): separate Log into RawLam namespace with conditional compiling

--- GluLamb/RawLam/Log.cs
+++ GluLamb/RawLam/Log.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿#if RAWLAM
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,7 @@ using System.Threading.Tasks;
 
 using Rhino.Geometry;
 
-namespace GluLamb
+namespace RawLam
 {
     public class Board
     {
@@ -107,3 +110,4 @@ namespace GluLamb
         }
     }
 }
+#endif
