c37635971a78cbd32dff04e1d09513dfaefc1cfa
Tom Svilans <email@example.com>
2022-04-21 09:53:49 +0200

    fix(projects): fixed CrossJointCutout.ToCix()

--- GluLamb/Projects/2022_HH_DAC.cs
+++ GluLamb/Projects/2022_HH_DAC.cs
@@ -206,11 +206,14 @@ namespace GluLamb.Projects.HHDAC22
 
         public override void ToCix(List<string> cix, string prefix = "")
         {
+            // Turn on joint
+            cix.Add(string.Format("{0}HAK_{1}=1", prefix, Id));
+
             // Write outline
             for (int i = 0; i < Outline.Count; ++i)
             {
-                cix.Add(string.Format("{0}_HAK_{1}_PKT_{2}_X={3:0.###}", prefix, Id, i + 1, Outline[i].X));
-                cix.Add(string.Format("{0}_HAK_{1}_PKT_{2}_Y={3:0.###}", prefix, Id, i + 1, Outline[i].Y));
+                cix.Add(string.Format("{0}HAK_{1}_PKT_{2}_X={3:0.###}", prefix, Id, i + 1, Outline[i].X));
+                cix.Add(string.Format("{0}HAK_{1}_PKT_{2}_Y={3:0.###}", prefix, Id, i + 1, Outline[i].Y));
             }
 
             // Write plane
@@ -224,14 +227,14 @@ namespace GluLamb.Projects.HHDAC22
 
             for (int i = 0; i < SideLines.Length; ++i)
             {
-                cix.Add(string.Format("{0}HAK_LINE_{1}_PKT_1_X={2:0.###}", prefix, i + 1, SideLines[i].From.X));
-                cix.Add(string.Format("{0}HAK_LINE_{1}_PKT_1_Y={2:0.###}", prefix, i + 1, SideLines[i].From.Y));
-                cix.Add(string.Format("{0}HAK_LINE_{1}_PKT_2_X={2:0.###}", prefix, i + 1, SideLines[i].To.X));
-                cix.Add(string.Format("{0}HAK_LINE_{1}_PKT_2_Y={2:0.###}", prefix, i + 1, SideLines[i].To.Y));
+                cix.Add(string.Format("{0}HAK_{1}_LINE_{2}_PKT_1_X={3:0.###}", prefix, Id, i + 1, SideLines[i].From.X));
+                cix.Add(string.Format("{0}HAK_{1}_LINE_{2}_PKT_1_Y={3:0.###}", prefix, Id, i + 1, SideLines[i].From.Y));
+                cix.Add(string.Format("{0}HAK_{1}_LINE_{2}_PKT_2_X={3:0.###}", prefix, Id, i + 1, SideLines[i].To.X));
+                cix.Add(string.Format("{0}HAK_{1}_LINE_{2}_PKT_2_Y={3:0.###}", prefix, Id, i + 1, SideLines[i].To.Y));
             }
 
-            cix.Add(string.Format("{0}HAK_DYBDE={1:0.###}", prefix, Depth));
-            cix.Add(string.Format("{0}HAK_ALFA={1:0.###}", prefix, Alpha));
+            cix.Add(string.Format("{0}HAK_{1}_DYBDE={2:0.###}", prefix, Id, Depth));
+            cix.Add(string.Format("{0}HAK_{1}_ALFA={2:0.###}", prefix, Id, Alpha));
         }
 
         public override void Transform(Transform xform)
