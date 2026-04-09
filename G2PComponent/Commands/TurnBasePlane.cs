47669c3a493763356e5f1b3e77f68bb6c17bc1b4
Tom Svilans <info@tomsvilans.com>
2026-04-09 12:22:26 +0200

    feat(commands): add roll and turn baseplane commands

---
+++ G2PComponent/Commands/TurnBasePlane.cs
@@ -0,0 +1,91 @@
+﻿using D2P_Core.Utility;
+using Rhino;
+using Rhino.Commands;
+using Rhino.DocObjects;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace G2PComponents.Commands
+{
+    public class TurnComponentPlaneCommand : Command
+    {
+        public TurnComponentPlaneCommand()
+        {
+            Instance = this;
+        }
+
+        public static TurnComponentPlaneCommand Instance
+        {
+            get; private set;
+        }
+
+        public override string EnglishName => "TurnComponentPlane";
+
+        private OptionToggle optionAxis = new OptionToggle(true, "YAxis", "XAxis");
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+
+            const ObjectType geometryFilter = ObjectType.AnyObject;
+
+            GetObject go = new GetObject();
+            go.SetCommandPrompt("Select components");
+            go.GeometryFilter = geometryFilter;
+            //go.AddOptionToggle("Axis", ref optionAxis);
+
+            go.GroupSelect = true;
+            go.SubObjectSelect = false;
+            go.EnableClearObjectsOnEntry(false);
+            go.EnableUnselectObjectsOnExit(false);
+            go.DeselectAllBeforePostSelect = false;
+            go.EnablePostSelect(true);
+            go.AcceptNothing(true);
+
+            for (; ; )
+            {
+                GetResult res = go.GetMultiple(1, 0);
+
+                if (res == GetResult.Option)
+                {
+                    go.EnablePreSelect(false, true);
+                    continue;
+                }
+
+                else if (res != GetResult.Object)
+                    return Result.Cancel;
+
+                if (go.ObjectsWerePreselected)
+                {
+                    //bHavePreselectedObjects = true;
+                    go.EnablePreSelect(false, true);
+                    continue;
+                }
+
+                break;
+            }
+
+            foreach (var rhObject in go.Objects())
+            {
+                rhObject.Object().Select(true, true);
+            }
+
+            var components = Instantiation.InstancesFromObjects(go.Objects().Select(x => x.Object()), Context.settings, doc);
+            foreach (var component in components)
+            {
+                var plane = component.Plane;
+                var brep = Utility.GetMember(component, "Geometry").First() as Brep;
+
+                if (brep == null) continue;
+
+                var turned = Utility.TurnBaseplane(brep, plane, true);
+                var label = component.Label.Duplicate() as TextEntity;
+                label.Plane = turned;
+
+                doc.Objects.Replace(component.ID, label);
+            }
+
+            return Result.Success;
+        }
+    }
+}
